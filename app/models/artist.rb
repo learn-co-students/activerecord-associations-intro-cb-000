0ab087b2906da95c4b5ece0c7e4f735e28aa1e9a
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 02:58:26 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ActiveRecord::Base
+  # tell the Artist class that each artist object can have many songs
+  has_many :songs
+  # an artist has many genres through songs
+  has_many :genres, through: :songs
 end
