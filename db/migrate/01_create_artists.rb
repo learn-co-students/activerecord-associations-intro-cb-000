ee8f99699c15d02677d43b06a295b2d02871c79f
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 02:52:39 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
+
+=begin
+  • An artist will have many songs and it will have many genres through songs
+=end
