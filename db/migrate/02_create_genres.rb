6d93c26997b79084f393ad94e6823ed79acefa51
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 02:53:39 +0000

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,11 @@
 class CreateGenres < ActiveRecord::Migration
+  def change
+    create_table :genres do |t|
+      t.string :name
+    end
+  end
 end
+
+=begin
+  • A genre will have many songs and it will have many artists through songs
+=end
