5eb509d17f80e7ab398fcc0724102560f4d45757
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-09-23 20:48:57 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def build(args = {})
+    Song.new(args)
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
 
+  def build_genre(args = {})
+    self.genre = Genre.create(args)
+  end
 end
